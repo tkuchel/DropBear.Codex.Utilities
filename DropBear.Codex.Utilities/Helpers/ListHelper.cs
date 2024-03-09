5a668dc053b7df0fca98de1d126377b4629d6795
DropBear <tkuchel@users.noreply.github.com>
2024-03-09 16:08:12 +1100

    Added a list helper.

---
+++ DropBear.Codex.Utilities/Helpers/ListHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+namespace DropBear.Codex.Utilities.Helpers;
+
+/// <summary>
+///     Provides extension methods for working with <see cref="List{T}" /> instances.
+/// </summary>
+public static class ListHelper
+{
+    /// <summary>
+    ///     Creates a read-only wrapper around the specified <see cref="List{T}" /> instance.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the list.</typeparam>
+    /// <param name="list">The source <see cref="List{T}" /> instance.</param>
+    /// <returns>A <see cref="ReadOnlyCollection{T}" /> that wraps the source list.</returns>
+    public static ReadOnlyCollection<T> ToReadOnlyCollection<T>(this List<T> list) => new(list);
+}
