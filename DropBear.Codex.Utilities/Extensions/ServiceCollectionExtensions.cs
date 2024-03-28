1a4338f0661f2ccd385bbb31cff49fc46e382c68
DropBear <tkuchel@users.noreply.github.com>
2024-03-28 12:07:59 +1100

    Added ServiceCollectionExtensions.cs with the ability to add the two hashing algorithms to the service collection for DI.

---
+++ DropBear.Codex.Utilities/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using DropBear.Codex.Utilities.Hashing;
+using DropBear.Codex.Utilities.Hashing.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DropBear.Codex.Utilities.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    ///     Adds the DropBear Codex utility services to the specified <see cref="IServiceCollection" />.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection" /> to add the services to.</param>
+    public static void AddHashingUtilities(this IServiceCollection services)
+    {
+        services.AddTransient<IPasswordHasher, BlakePasswordHasher>();
+        services.AddTransient<IPasswordHasher, ArgonPasswordHasher>();
+    }
+    
+}
