c9f9c79db88806a3fb8071643c1d9ee8f4b5ab8a
DropBear <tkuchel@users.noreply.github.com>
2024-04-01 14:58:29 +1100

    Make hashing services Keyed. Added serviceprovider for keyed services.

--- DropBear.Codex.Utilities/Extensions/ServiceCollectionExtensions.cs
+++ DropBear.Codex.Utilities/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using DropBear.Codex.Utilities.Hashing;
 using DropBear.Codex.Utilities.Hashing.Interfaces;
+using DropBear.Codex.Utilities.Hashing.Providers;
 using DropBear.Codex.Utilities.MessageTemplates;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -13,15 +14,15 @@ public static class ServiceCollectionExtensions
     /// <param name="services">The <see cref="IServiceCollection" /> to add the services to.</param>
     public static void AddHashingUtilities(this IServiceCollection services)
     {
-        services.AddTransient<IHashingService, Blake2HashingService>();
-        services.AddTransient<IHashingService, Argon2HashingService>();
-        services.AddTransient<IHashingService, Blake3HashingService>();
-        services.AddTransient<IHashingService, Fnv1AHashingService>();
-        services.AddTransient<IHashingService, MurmurHash3Service>();
-        services.AddTransient<IHashingService, SipHashingService>();
-        services.AddTransient<IHashingService, XxHashingService>();
+        services.AddKeyedTransient<IHashingService,Blake2HashingService>("Blake2");
+        services.AddKeyedTransient<IHashingService, Argon2HashingService>("Argon2");
+        services.AddKeyedTransient<IHashingService, Blake3HashingService>("Blake3");
+        services.AddKeyedTransient<IHashingService, Fnv1AHashingService>("Fnv1A");
+        services.AddKeyedTransient<IHashingService, MurmurHash3Service>("Murmur3");
+        services.AddKeyedTransient<IHashingService, SipHashingService>("SipHash");
+        services.AddKeyedTransient<IHashingService, XxHashingService>("XXHash");
+        services.AddSingleton<IHashingServiceProvider, HashingServiceProvider>();
 
-       
     }
     
     /// <summary>
---
+++ DropBear.Codex.Utilities/Hashing/Interfaces/IHashingServiceProvider.cs
@@ -0,0 +1,6 @@
+﻿namespace DropBear.Codex.Utilities.Hashing.Interfaces;
+
+public interface IHashingServiceProvider
+{
+    IHashingService GetHashingService(string key);
+}
