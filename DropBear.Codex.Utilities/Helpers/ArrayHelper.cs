054b4f2db86b49d420011bbdd8a3a26ba19884ec
DropBear <tkuchel@users.noreply.github.com>
2024-04-20 12:17:41 +1000

    Added ArrayHelper.cs with a PrintByteArray method.

---
+++ DropBear.Codex.Utilities/Helpers/ArrayHelper.cs
@@ -0,0 +1,36 @@
+﻿namespace DropBear.Codex.Utilities.Helpers;
+
+public static class ArrayHelper
+{
+    public static void PrintByteArray(byte[] bytes)
+    {
+        const int BytesPerLine = 16;
+        var bytesRemaining = bytes.Length;
+        var bytesRead = 0;
+
+        while (bytesRemaining > 0)
+        {
+            Console.Write("{0:X8}: ", bytesRead);
+
+            var bytesToPrint = Math.Min(BytesPerLine, bytesRemaining);
+
+            for (var i = 0; i < bytesToPrint; i++) Console.Write("{0:X2} ", bytes[bytesRead + i]);
+
+            for (var i = bytesToPrint; i < BytesPerLine; i++) Console.Write("   ");
+
+            Console.Write(" ");
+
+            for (var i = 0; i < bytesToPrint; i++)
+            {
+                var b = bytes[bytesRead + i];
+                var c = b < 32 || b > 126 ? '.' : (char)b;
+                Console.Write(c);
+            }
+
+            Console.WriteLine();
+
+            bytesRead += bytesToPrint;
+            bytesRemaining -= bytesToPrint;
+        }
+    }
+}
