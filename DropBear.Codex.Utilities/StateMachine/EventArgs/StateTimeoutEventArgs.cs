06260e3fa4a93bd8e97aa998ca883881f150f20e
DropBear <tkuchel@users.noreply.github.com>
2024-04-23 19:29:22 +1000

    Added a simple generic statemachine implementation to the utilities project.

---
+++ DropBear.Codex.Utilities/StateMachine/EventArgs/StateTimeoutEventArgs.cs
@@ -0,0 +1,9 @@
+﻿namespace DropBear.Codex.Utilities.StateMachine.EventArgs;
+
+// Custom EventArgs to carry state information
+public class StateTimeoutEventArgs<TState> : System.EventArgs where TState : Enum
+{
+    public StateTimeoutEventArgs(TState nextState) => NextState = nextState;
+
+    public TState NextState { get; }
+}
---
+++ DropBear.Codex.Utilities/StateMachine/Interfaces/IState.cs
@@ -0,0 +1,11 @@
+﻿namespace DropBear.Codex.Utilities.StateMachine.Interfaces;
+
+public interface IState<out TState> where TState : Enum
+{
+    TState StateKey { get; }
+    void EnterState();
+    void ExitState();
+    void UpdateState();
+    TState GetNextState();
+    void ResetState(); 
+}
