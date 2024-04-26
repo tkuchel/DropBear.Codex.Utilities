6036d73ed151a93f65b443fbdcbbc7ba62f7a25e
DropBear <tkuchel@users.noreply.github.com>
2024-04-26 15:20:27 +1000

    Removed hashing features from the Utilities library, this is now a standalone library DropBear.Codex.Hashing.

--- DropBear.Codex.Utilities/Extensions/ServiceCollectionExtensions.cs
+++ DropBear.Codex.Utilities/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,4 @@
 ﻿using DropBear.Codex.Utilities.FeatureFlags;
-using DropBear.Codex.Utilities.Hashing.Builder;
-using DropBear.Codex.Utilities.Hashing.Interfaces;
 using DropBear.Codex.Utilities.MessageTemplates;
 using DropBear.Codex.Utilities.MessageTemplates.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,18 +7,12 @@ namespace DropBear.Codex.Utilities.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddHashingServices(this IServiceCollection services)
-    {
-        services.AddSingleton<IHashBuilder, HashBuilder>();
-        return services;
-    }
-    
     public static IServiceCollection AddDynamicFlagManager(this IServiceCollection services)
     {
         services.AddSingleton<IDynamicFlagManager, DynamicFlagManager>();
         return services;
     }
-    
+
     public static IServiceCollection AddMessageTemplateManager(this IServiceCollection services)
     {
         services.AddSingleton<IMessageTemplateProvider, MessageTemplateProvider>();
--- DropBear.Codex.Utilities/Hashing/Interfaces/IHashBuilder.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DropBear.Codex.Utilities.Hashing.Interfaces;
-
-public interface IHashBuilder
-{
-    IHasher GetHasher(string key);
-}
