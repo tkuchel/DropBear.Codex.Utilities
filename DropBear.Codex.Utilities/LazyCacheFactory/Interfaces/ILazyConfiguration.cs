1092c21c87d9a3ce2bbc883c130610f589d92d14
DropBear <tkuchel@users.noreply.github.com>
2024-05-02 13:30:24 +1000

    Forgot to update the interface for the new methods.

--- DropBear.Codex.Utilities/LazyCacheFactory/Interfaces/ILazyConfiguration.cs
+++ DropBear.Codex.Utilities/LazyCacheFactory/Interfaces/ILazyConfiguration.cs
@@ -1,8 +1,12 @@
-﻿namespace DropBear.Codex.Utilities.LazyCacheFactory.Interfaces;
+﻿using DropBear.Codex.Utilities.LazyCacheFactory.Models;
+
+namespace DropBear.Codex.Utilities.LazyCacheFactory.Interfaces;
 
 public interface ILazyConfiguration<T>
 {
     ILazyConfiguration<T> WithInitialization(Func<T> initializer);
+    ILazyConfiguration<T> WithAsyncInitialization(Func<Task<T>> asyncInitializer);
     ILazyConfiguration<T> WithCaching(TimeSpan expiration);
     Lazy<T> Build();
+    AsyncLazy<T> BuildAsync();
 }
