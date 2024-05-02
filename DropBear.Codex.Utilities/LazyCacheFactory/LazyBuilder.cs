4a6be6ff145720bcd5db55984729e38387a36fb9
DropBear <tkuchel@users.noreply.github.com>
2024-05-02 14:16:03 +1000

    Added an overload of build with forced sync for some very specific circumstances.

--- DropBear.Codex.Utilities/LazyCacheFactory/LazyBuilder.cs
+++ DropBear.Codex.Utilities/LazyCacheFactory/LazyBuilder.cs
@@ -3,60 +3,104 @@ using DropBear.Codex.Utilities.LazyCacheFactory.Models;
 
 namespace DropBear.Codex.Utilities.LazyCacheFactory;
 
+/// <summary>
+///     Provides a builder for creating lazy instances of type <typeparamref name="T" />, supporting both synchronous and
+///     asynchronous initialization.
+/// </summary>
+/// <typeparam name="T">The type of object to be lazily instantiated.</typeparam>
 public class LazyBuilder<T> : ILazyConfiguration<T>
 {
     private readonly MemoryCacheManager _cacheManager;
-
-    private Func<Task<T>> _asyncInitializer =
-        () => throw new InvalidOperationException("Initialization function not set.");
-
+    private Func<Task<T>> _asyncInitializer;
     private TimeSpan? _cacheExpiration;
-    private Func<T> _initializer = () => throw new InvalidOperationException("Initialization function not set.");
+    private Func<T> _initializer;
 
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="LazyBuilder{T}" /> class.
+    /// </summary>
+    /// <param name="cacheManager">The cache manager used to manage caching for lazy instances.</param>
     public LazyBuilder(MemoryCacheManager cacheManager) => _cacheManager = cacheManager;
 
+    /// <summary>
+    ///     Specifies the initialization function for the lazy instance.
+    /// </summary>
+    /// <param name="initializer">A function that returns an instance of <typeparamref name="T" />.</param>
+    /// <returns>The current instance of <see cref="LazyBuilder{T}" />.</returns>
     public ILazyConfiguration<T> WithInitialization(Func<T> initializer)
     {
         _initializer = initializer;
         return this;
     }
 
-    public ILazyConfiguration<T> WithCaching(TimeSpan expiration = default)
-    {
-        // Set the default expiration to 5 minutes if no expiration time is provided
-        // or if the provided expiration is the default value for TimeSpan (which is zero).
-        _cacheExpiration = expiration == default ? TimeSpan.FromMinutes(5) : expiration;
-        return this;
-    }
-
-
-    public Lazy<T> Build()
-    {
-        if (_cacheExpiration is null)
-            throw new InvalidOperationException("Cache expiration not set.");
-
-        if (_asyncInitializer is not null)
-            throw new InvalidOperationException("Build method does not support asynchronous initializers.");
-        return new Lazy<T>(() => _cacheExpiration.HasValue
-            ? _cacheManager.GetOrCreate(typeof(T).ToString(), _initializer, _cacheExpiration)
-            : _initializer());
-    }
-
+    /// <summary>
+    ///     Specifies the asynchronous initialization function for the lazy instance.
+    /// </summary>
+    /// <param name="asyncInitializer">A function that returns a Task of <typeparamref name="T" />.</param>
+    /// <returns>The current instance of <see cref="LazyBuilder{T}" />.</returns>
     public ILazyConfiguration<T> WithAsyncInitialization(Func<Task<T>> asyncInitializer)
     {
         _asyncInitializer = asyncInitializer;
         return this;
     }
 
+    /// <summary>
+    ///     Specifies the cache expiration for the lazy instance.
+    /// </summary>
+    /// <param name="expiration">The timespan after which the cache should expire.</param>
+    /// <returns>The current instance of <see cref="LazyBuilder{T}" />.</returns>
+    public ILazyConfiguration<T> WithCaching(TimeSpan expiration = default)
+    {
+        _cacheExpiration = expiration == default ? TimeSpan.FromMinutes(5) : expiration;
+        return this;
+    }
+
+    /// <summary>
+    ///     Builds the lazy instance using the specified initialization function.
+    /// </summary>
+    /// <exception cref="InvalidOperationException">Thrown if no initialization function has been set.</exception>
+    /// <returns>A lazy wrapper around the initialization function.</returns>
+    public Lazy<T> Build()
+    {
+        if (_initializer is null)
+            throw new InvalidOperationException("Synchronous initializer must be set.");
+
+        return new Lazy<T>(() => _cacheExpiration.HasValue
+            ? _cacheManager.GetOrCreate(typeof(T).ToString(), _initializer, _cacheExpiration)
+            : _initializer());
+    }
+
+    /// <summary>
+    ///     Builds the asynchronous lazy instance using the specified asynchronous initialization function.
+    /// </summary>
+    /// <exception cref="InvalidOperationException">Thrown if no asynchronous initialization function has been set.</exception>
+    /// <returns>An asynchronous lazy wrapper around the initialization function.</returns>
     public AsyncLazy<T> BuildAsync()
     {
-        if (_cacheExpiration is null)
-            throw new InvalidOperationException("Cache expiration not set.");
+        if (_asyncInitializer is null)
+            throw new InvalidOperationException("Asynchronous initializer must be set.");
 
-        if (_initializer is not null)
-            throw new InvalidOperationException("BuildAsync method does not support synchronous initializers.");
         return new AsyncLazy<T>(() => _cacheExpiration.HasValue
             ? _cacheManager.GetOrCreateAsync(typeof(T).ToString(), _asyncInitializer, _cacheExpiration)
             : _asyncInitializer());
+    }
+
+    /// <summary>
+    ///     Builds a Lazy&lt;T&gt; instance using a potentially asynchronous initialization function,
+    ///     forcing it to run synchronously. Use with caution as this can lead to deadlocks and performance issues.
+    /// </summary>
+    /// <returns>A Lazy&lt;T&gt; that will initialize the value on first access.</returns>
+    /// <exception cref="InvalidOperationException">Thrown if no asynchronous initializer is set.</exception>
+    public Lazy<T> BuildWithForcedSync()
+    {
+        if (_asyncInitializer is null)
+            throw new InvalidOperationException("Asynchronous initializer must be set.");
+
+        return new Lazy<T>(() =>
+        {
+            // Using Task.Run to offload to the thread pool and calling .Result to force synchronization
+            return Task.Run(async () => await (_cacheExpiration.HasValue
+                ? _cacheManager.GetOrCreateAsync(typeof(T).ToString(), _asyncInitializer, _cacheExpiration)
+                : _asyncInitializer()).ConfigureAwait(false)).Result;
+        });
     }
 }
